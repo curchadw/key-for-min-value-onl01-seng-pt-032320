82d2944408fbe57bd91738b7e6d8f58cc07e4308
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-27 21:39:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,8 @@ def key_for_min_value(name_hash)
   min_key = nil
   name_hash.each do |key, value|
       if min_value = nil
-        min_
+        min_value = value
+        min_key = key
+      elsif min_value >
   end
 end
